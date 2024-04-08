1da983f5bb412f5e385581536329f298d3361628
HR Bodvarsson <hrbodvarsson@gmail.com>
2024-04-08 18:27:53 +0000

    v1

---
+++ lib/main3.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+//function body
+//functions are in the end 'instructions' but they are not executed by the program
+// unless called
+//functions: "Code on demand"
+  runApp(const MaterialApp(home: Text('Hello World!')));
+  //arguments = calling a function
+  //parameters = defining a function
+  //some functions like void do not need input values, but rather a widget tree
+  //widget tree = widgets nested into each other
+
+
+  //const = helps Dart optimize runtime performance
+}
+
