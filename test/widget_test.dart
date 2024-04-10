7796da28c0d7d761aebe6f0cfc15f689077d40a0
HR Bodvarsson <hrbodvarsson@gmail.com>
2024-04-10 21:54:16 +0000

    as to vs
    asdf

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test1flutter/main.dart';
+import 'package:test1flutter/main4.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
